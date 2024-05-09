da7db9edf251eff7b7bc18abb4ed9b4150c0df4f
Vasiliy Zazdravnykh <coder.vz@gmail.com>
2024-05-09 18:41:57 +0300

    Закрыт доступ для неакторизованного пользователя

--- FRONT/src/components/Expenses/index.tsx
+++ FRONT/src/components/Expenses/index.tsx
@@ -20,6 +20,7 @@ const Expenses = () => {
     const indexDate = getMonth(new Date()) + 1
     const currentMonth = months[indexDate]
     const [defaultMonth, setDefaultMonth] = useState(currentMonth)
+    const [currentUser, setCurrentUser] = useState(undefined);
 
     useEffect(() => {
         setDefaultMonth(currentMonth)
@@ -35,43 +36,43 @@ const Expenses = () => {
 
     return (
         <div>
-            <h1 className="title">Учет расходов</h1>
-            <Form func={setPurchases} data={purchases} options={options} />
-            <Statistics
-                title="Статистика расходов"
-                copyData={filteredPurchase}
-                options={options}
-            />
-            <div className="flex flex-col py-10">
-                <Months
-                    data={purchases}
-                    func={setFilteredPurchase}
-                    defaultMonth={defaultMonth}
-                    setDefaultMonth={setDefaultMonth}
-                />
-                <DeleteModal
-                    data={purchases}
-                    func={setPurchases}
-                    defaultMonth={defaultMonth}
-                />
-                {filteredPurchase.length === 0 && (
-                    <div className="text-center font-semibold text-xl pt-16 pb-4">
-                        Нет внесенных расходов
-                    </div>
-                )}
-                {filteredPurchase.length > 0 && (
-                    <div className="rounded-t-md overflow-hidden">
-                        {filteredPurchase.map((purchase: any) => (
-                            <ElementCard
-                                dataElem={purchase}
-                                key={purchase.id}
-                                data={purchases}
-                                func={setPurchases}
-                            />
-                        ))}
-                    </div>
-                )}
-            </div>
+            {currentUser ? (
+                <><h1 className="title">Учет расходов</h1><Form func={setPurchases} data={purchases} options={options} /><Statistics
+                    title="Статистика расходов"
+                    copyData={filteredPurchase}
+                    options={options} /><div className="flex flex-col py-10">
+                        <Months
+                            data={purchases}
+                            func={setFilteredPurchase}
+                            defaultMonth={defaultMonth}
+                            setDefaultMonth={setDefaultMonth} />
+                        <DeleteModal
+                            data={purchases}
+                            func={setPurchases}
+                            defaultMonth={defaultMonth} />
+                        {filteredPurchase.length === 0 && (
+                            <div className="text-center font-semibold text-xl pt-16 pb-4">
+                                Нет внесенных расходов
+                            </div>
+                        )}
+                        {filteredPurchase.length > 0 && (
+                            <div className="rounded-t-md overflow-hidden">
+                                {filteredPurchase.map((purchase: any) => (
+                                    <ElementCard
+                                        dataElem={purchase}
+                                        key={purchase.id}
+                                        data={purchases}
+                                        func={setPurchases} />
+                                ))}
+                            </div>
+                        )}
+                    </div></>
+            ) : (
+                <div>
+                    <p>Вы не авторизованы...</p>
+                    <p>Войдите или зарегистрируйтесь.</p>
+                </div>
+            )}
         </div>
     )
 }
